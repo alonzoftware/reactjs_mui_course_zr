b87e0ba01271f56e0867d9d2a355b4f6f233415f
HPLap <alonzoftware@gmail.com>
2023-05-24 12:13:10 -0400

    HPLap 20230524 12:12 01-Headers-Navigation :: First Commit

---
+++ 01-Headers-Navigation/src/components/ui/Header.tsx
@@ -0,0 +1,32 @@
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+export const Header = () => {
+    const onCLickBox = () => {
+        console.log('Here your Text');
+
+    }
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="fixed">
+                <Toolbar variant="dense">
+                    <IconButton
+                        onClick={onCLickBox}
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}>
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" color="inherit" component="div">
+                        SISBADI
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    )
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# MATERIAL UI
+
+https://mui.com/
+
+```console
+npm install @mui/material @emotion/react @emotion/styled
+npm install @mui/icons-material
+```
